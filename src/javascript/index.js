736a6100b9c747d26fb6f351e44d8face7f37cdb
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-31 00:15:40 -0500

    when page load, load home page

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap styles
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS components
 import "../assets/css/styles.css"; // Adjusted path to styles.css
+import loadHomepage from "/src/javascript/loadHomePage.js"; // Load Home Page
 
 //------ NAVIGATION Logo
 // Importing food logo for tha navigation
@@ -8,3 +9,9 @@ import javascriptLogo from "../assets/img/foodLogo.png";
 // Example of dynamically setting the image in the navbar
 const imageElement = document.querySelector("#companyLogo");
 imageElement.src = javascriptLogo;
+
+// Initial Page Load
+document.addEventListener("DOMContentLoaded", () => {
+  loadHomepage(); // Load Home by default
+  setupNavigation(); // Make Navigation Btn work.
+});
