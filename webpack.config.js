742353ea0f2a87b18bfa162739b9078da37aa8d3
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-30 23:52:58 -0500

    added rules for images

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,8 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Handle images
+        type: "asset/resource",
       },
     ],
   },
