3383ddfb81e21e3b51d97af4d5ae81dfd131bc13
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-31 00:21:59 -0500

    create event listeners when click

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -15,3 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
   loadHomepage(); // Load Home by default
   setupNavigation(); // Make Navigation Btn work.
 });
+
+//-- Setting up Functionality to btns on Navigation
+function setupNavigation() {
+  // Grabbing the div#content
+  const content = document.getElementById("content");
+  // Home Button
+  const homeBTN = document.querySelector(".btn-primary");
+  // Menu Button
+  const menuBTN = document.querySelector(".btn-success");
+  // Contact Button
+  const contactBTN = document.querySelector(".btn-warning");
+
+  // Clear Existing Content
+  function clearContent() {
+    content.textContent = "";
+  }
+
+  //-- Listeners for navigation Buttons
+  // Home Page
+  homeBTN.addEventListener("click", () => {
+    clearContent();
+    // load the homepage content
+    loadHomepage();
+  });
+  // Menu Page
+  menuBTN.addEventListener("click", () => {
+    clearContent();
+    alert("menu Button");
+  });
+  // Contact Page
+  contactBTN.addEventListener("click", () => {
+    clearContent();
+    alert("contact Button");
+  });
+}
