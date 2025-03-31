fff46639905ac06ca4151c86c3c812d997b8cf55
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-31 00:24:23 -0500

    added data of meals we have

---
+++ src/javascript/menuData.js
@@ -0,0 +1,55 @@
+import menuItem1 from "../assets/img/menu-item-1.png";
+import menuItem2 from "../assets/img/menu-item-2.png";
+import menuItem3 from "../assets/img/menu-item-3.png";
+import menuItem4 from "../assets/img/menu-item-4.png";
+import menuItem5 from "../assets/img/menu-item-5.png";
+import menuItem6 from "../assets/img/menu-item-6.png";
+
+export const menuItems = [
+  {
+    category: "Lunch",
+    items: [
+      {
+        name: "Magnam Tiste",
+        description: "Lorem, deren, tratoro, filede, nerada",
+        price: "5.95",
+        image: menuItem1,
+      },
+      {
+        name: "aut Luia",
+        description: "Lorem, deren, tratoro, filede, nerada",
+        price: "14.95",
+        image: menuItem2,
+      },
+      {
+        name: "Est Eligendi",
+        description: "Lorem, deren, tratoro, filede, nerada",
+        price: "8.95",
+        image: menuItem3,
+      },
+    ],
+  },
+  {
+    category: "Dinner",
+    items: [
+      {
+        name: "Laboriosam Direva",
+        description: "Lorem, deren, trataro, filede, nerada",
+        price: "$9.95",
+        image: menuItem4,
+      },
+      {
+        name: "Eos Luibusdam",
+        description: "Lorem, deren, trataro, filede, nerada",
+        price: "$12.95",
+        image: menuItem5,
+      },
+      {
+        name: "aut Luia",
+        description: "Lorem, deren, tratoro, filede, nerada",
+        price: "14.95",
+        image: menuItem6,
+      },
+    ],
+  },
+];
