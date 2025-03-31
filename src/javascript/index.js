f222dade0a05f883714d0a7d4a37bbe16e5b8116
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-30 23:54:34 -0500

    showcase navbar logo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap styles
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS components
 import "../assets/css/styles.css"; // Adjusted path to styles.css
+
+//------ NAVIGATION Logo
+// Importing food logo for tha navigation
+import javascriptLogo from "../assets/img/foodLogo.png";
+// Example of dynamically setting the image in the navbar
+const imageElement = document.querySelector("#companyLogo");
+imageElement.src = javascriptLogo;
